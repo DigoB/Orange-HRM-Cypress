c8a1252b5d1b98aa58030471c142fe1c0ea3f55b
Rodrigo Braz <braz.rodrigo94@gmail.com>
2025-10-10 17:53:21 -0300

    Login Fail tests

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -7,4 +7,23 @@ describe('Orange HRM Tests', () => {
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
   })
+  it('Login - Failed - Wrong Password', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('wrongpassword')
+    cy.get('.oxd-button').click()
+    cy.get('.oxd-alert-content > .oxd-text').contains('Invalid credentials')
+  })
+  it('Login - Failed - Empty Fields', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get('.oxd-button').click()
+    cy.get('.oxd-input-group > .oxd-text').contains('Required')
+  })
+  it('Login - Failed - Wrong Username', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('WrongUser')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get('.oxd-button').click()
+    cy.get('.oxd-alert-content > .oxd-text').contains('Invalid credentials')
+  })
 })
