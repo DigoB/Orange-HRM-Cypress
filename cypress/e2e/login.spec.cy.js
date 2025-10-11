7caa4975cd03caf360fe98fa4280e8a14c0e4a55
Rodrigo Braz <braz.rodrigo94@gmail.com>
2025-10-11 13:37:34 -0300

    Adding user data json

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,5 +1,6 @@
-describe('Orange HRM Tests', () => {
+import userData from "../fixtures/user-data.json";
 
+describe("Orange HRM Tests", () => {
   const selectorsList = {
     usernameInput: "[name='username']",
     passwordInput: "[name='password']",
@@ -7,34 +8,42 @@ describe('Orange HRM Tests', () => {
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
     wrongCredentialsError: "[role='alert']",
     emptyFieldsError: "cy.get(':nth-child(2) > .oxd-input-group > .oxd-text')",
-    dashboardGrid: ".orangehrm-dashboard-grid"
-  }
+    dashboardGrid: ".orangehrm-dashboard-grid",
+  };
 
-  it('Login - Success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameInput).type('Admin')
-    cy.get(selectorsList.passwordInput).type('admin123')
-    cy.get(selectorsList.submitButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.dashboardGrid)
-  })
-  it('Login - Failed - Wrong Password', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameInput).type('Admin')
-    cy.get(selectorsList.passwordInput).type('wrongpassword')
-    cy.get(selectorsList.submitButton).click()
-    cy.get(selectorsList.wrongCredentialsError).contains('Invalid credentials')
-  })
-  it('Login - Failed - Empty Fields', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.submitButton).click()
-    cy.get('.oxd-input-group > .oxd-text').contains('Required')
-  })
-  it('Login - Failed - Wrong Username', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameInput).type('WrongUser')
-    cy.get(selectorsList.passwordInput).type('admin123')
-    cy.get(selectorsList.submitButton).click()
-    cy.get(selectorsList.wrongCredentialsError).contains('Invalid credentials')
-  })
-})
+  it("Login - Success", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+    cy.get(selectorsList.usernameInput).type(userData.userSuccess.username);
+    cy.get(selectorsList.passwordInput).type(userData.userSuccess.password);
+    cy.get(selectorsList.submitButton).click();
+    cy.location("pathname").should("equal", "/web/index.php/dashboard/index");
+    cy.get(selectorsList.dashboardGrid);
+  });
+  it("Login - Failed - Wrong Username", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+    cy.get(selectorsList.usernameInput).type(userData.userFail.username);
+    cy.get(selectorsList.passwordInput).type(userData.userSuccess.password);
+    cy.get(selectorsList.submitButton).click();
+    cy.get(selectorsList.wrongCredentialsError).contains("Invalid credentials");
+  });
+  it("Login - Failed - Wrong Password", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+    cy.get(selectorsList.usernameInput).type(userData.userSuccess.username);
+    cy.get(selectorsList.passwordInput).type(userData.userFail.password);
+    cy.get(selectorsList.submitButton).click();
+    cy.get(selectorsList.wrongCredentialsError).contains("Invalid credentials");
+  });
+  it("Login - Failed - Empty Fields", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+    cy.get(selectorsList.submitButton).click();
+    cy.get(".oxd-input-group > .oxd-text").contains("Required");
+  });
+});
